05e913d4e5da989854d01e1282874eb6296f5c65
Maruf <marufzoiroff@gmail.com>
2022-04-13 09:17:20 +0500

    created configurationProviders

---
+++ CustomConfiguration/ConfigurationProviders/IConfigurationProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace CustomConfiguration.ConfigurationProviders
+{
+    public interface IConfigurationProvider
+    {
+        void SaveConfiguration(Dictionary<string, string> configuration);
+        Dictionary<string, string> LoadConfiguration();
+    }
+}
---
+++ CustomConfiguration/ConfigurationProviders/XmlConfigurationProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomConfiguration.ConfigurationProviders
+{
+    public class XmlConfigurationProvider : IConfigurationProvider
+    {
+        private readonly string _path;
+
+        public XmlConfigurationProvider(string path)
+        {
+            _path = path;
+        }
+
+        public Dictionary<string, string> LoadConfiguration()
+        {
+            return new Dictionary<string, string> { { "integer", "1" } };
+        }
+
+        public void SaveConfiguration(Dictionary<string, string> configuration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
