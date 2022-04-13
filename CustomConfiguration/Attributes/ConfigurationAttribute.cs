83f6602b788a83c735b24507e3fab1e354a09277
Maruf <marufzoiroff@gmail.com>
2022-04-13 14:35:49 +0500

    initialized section property from constructor

--- CustomConfiguration/Attributes/ConfigurationAttribute.cs
+++ CustomConfiguration/Attributes/ConfigurationAttribute.cs
@@ -7,12 +7,13 @@ namespace CustomConfiguration.Attributes
     public class ConfigurationAttribute : Attribute
     {
         public string Path { get; private set; }
-        public string Section { get; set; }
+        public string Section { get; private set; }
         public ProviderType ProviderType { get; private set; }
 
-        public ConfigurationAttribute(string path, ProviderType providerType)
+        public ConfigurationAttribute(string path, ProviderType providerType, string section)
         {
             Path = path;
+            Section = section;
             ProviderType = providerType;
         }
     }
