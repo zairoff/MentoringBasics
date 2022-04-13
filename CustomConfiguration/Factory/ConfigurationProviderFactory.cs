85d97ce05b2cb3eb4e067788f167532217eef5a3
Maruf <marufzoiroff@gmail.com>
2022-04-13 14:28:52 +0500

    added JSON Provider

--- CustomConfiguration/Factory/ConfigurationProviderFactory.cs
+++ CustomConfiguration/Factory/ConfigurationProviderFactory.cs
@@ -5,12 +5,13 @@ namespace CustomConfiguration.Factory
 {
     public class ConfigurationProviderFactory
     {
-        public static IConfigurationProvider GetConfigurationProvider(ProviderType provider, string path)
+        public static IConfigurationProvider GetConfigurationProvider(ProviderType provider, string path, string section)
         {
             IConfigurationProvider configurationProvider;
             switch (provider)
             {
                 case ProviderType.Xml: configurationProvider = new XmlConfigurationProvider(path); break;
+                case ProviderType.Json: configurationProvider = new JsonConfigurationProvider(path, section); break;
                 default: configurationProvider = new XmlConfigurationProvider(path); break;
             }
 
