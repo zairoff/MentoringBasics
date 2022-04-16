a16678fcb1f3d214ecd1fd1256d1089ca3af451c
Maruf <marufzoiroff@gmail.com>
2022-04-16 09:58:51 +0500

    added DataStructs project. Implemented DoubleLinked List. All tests passed

---
+++ src/Module7/DataStructs/HybridFlowProcessor.cs
@@ -0,0 +1,28 @@
+﻿using DataStructs.DoNotChange;
+using System;
+
+namespace DataStructs
+{
+    public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
+    {
+        public T Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Enqueue(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
