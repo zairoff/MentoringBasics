c772ed21116abefd018248b2c8d22f4e7932c495
Maruf <marufzoiroff@gmail.com>
2022-04-16 10:02:35 +0500

    implemented HybridFlowProcessor class. All tests passed

--- src/Module7/DataStructs/HybridFlowProcessor.cs
+++ src/Module7/DataStructs/HybridFlowProcessor.cs
@@ -5,24 +5,47 @@ namespace DataStructs
 {
     public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
     {
+        private readonly DoublyLinkedList<T> _list;
+
+        public HybridFlowProcessor()
+        {
+            _list = new DoublyLinkedList<T>();
+        }
+
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var index = _list.Length == 0 ? 0 : _list.Length - 1;
+                return _list.RemoveAt(index);
+            }
+            catch
+            {
+                throw new InvalidOperationException();
+            }
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            var index = _list.Length == 0 ? 0 : _list.Length - 1;
+            _list.AddAt(index, item);
         }
 
         public T Pop()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _list.RemoveAt(0);
+            }
+            catch
+            {
+                throw new InvalidOperationException();
+            }
         }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            _list.AddAt(0, item);
         }
     }
 }
