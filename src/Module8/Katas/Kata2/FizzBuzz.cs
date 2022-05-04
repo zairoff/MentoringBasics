09adbb17bfe1129b4642145ff54d510bbe0053e3
Maruf <marufzoiroff@gmail.com>
2022-05-04 21:05:00 +0500

    created and implemented FizzBuzz class. All tests passed

---
+++ src/Module8/Katas/Kata2/FizzBuzz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Katas.Kata2
+{
+    public class FizzBuzz
+    {
+        public static string Print(string input)
+        {
+            var isNumeric = int.TryParse(input, out int number);
+
+            if (!isNumeric)
+                throw new InvalidOperationException("input string is not integer number");
+
+            if (number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
+
+            if (number % 3 == 0)
+                return "Fizz";
+
+            if (number % 5 == 0)
+                return "Buzz";
+
+            return number.ToString();
+        }
+    }
+}
---
+++ src/Module8/UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,50 @@
+﻿using Katas.Kata2;
+using System;
+using Xunit;
+
+namespace UnitTests
+{
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void CheckInteger_WhenIputDevisibleTo_3_ShouldReturnFizz()
+        {
+            var input = "27";
+            var actual = "Fizz";
+
+            var result = FizzBuzz.Print(input);
+
+            Assert.Equal(actual, result);
+        }
+
+        [Fact]
+        public void CheckInteger_WhenIputDevisibleTo_5_ShouldReturnBuzz()
+        {
+            var input = "25";
+            var actual = "Buzz";
+
+            var result = FizzBuzz.Print(input);
+
+            Assert.Equal(actual, result);
+        }
+
+        [Fact]
+        public void CheckInteger_WhenIputDevisibleTo_3_And_5_ShouldReturnFizzBuzz()
+        {
+            var input = "45";
+            var actual = "FizzBuzz";
+
+            var result = FizzBuzz.Print(input);
+
+            Assert.Equal(actual, result);
+        }
+
+        [Fact]
+        public void CheckInteger_WhenIputIsNotIntegerNumber_ShouldThrowException()
+        {
+            var input = "10.4";
+
+            Assert.Throws<InvalidOperationException>(() => FizzBuzz.Print(input));
+        }
+    }
+}
