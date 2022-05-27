b55f16d99f11bae6e491d4ec1de58b08fc6698c3
Maruf <marufzoiroff@gmail.com>
2022-05-27 15:08:03 +0500

    implemented FileSystem class to work with file system & stream

---
+++ Serialization/Contract/IFileSystem.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace Serialization.Contract
+{
+    public interface IFileSystem
+    {
+        FileStream CreateFileStream(string path);
+
+        FileStream OpenFileStream(string path);
+    }
+}
---
+++ Serialization/FileSystem.cs
@@ -0,0 +1,18 @@
+﻿using Serialization.Contract;
+using System.IO;
+
+namespace Serialization
+{
+    public class FileSystem : IFileSystem
+    {
+        public FileStream CreateFileStream(string path)
+        {
+            return new FileStream(path, FileMode.Create);
+        }
+
+        public FileStream OpenFileStream(string path)
+        {
+           return new FileStream(path, FileMode.Open);
+        }
+    }
+}
