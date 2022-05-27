f692e9438826efd4d561e1d97d8b6b655c0ef14d
Maruf <marufzoiroff@gmail.com>
2022-05-27 15:47:41 +0500

    created test method for JsonSerializer, test passed

---
+++ SerializationTest/JSONSerializerTest.cs
@@ -0,0 +1,32 @@
+﻿using Serialization;
+using Serialization.Model;
+using System.Collections.Generic;
+using Xunit;
+
+namespace SerializationTest
+{
+    public class JSONSerializerTest
+    {
+        [Fact]
+        public void Serialize_And_Deserialize_Should_Serialize_Object_AsBinary_And_Deserialize()
+        {
+            var departmnent = new Department
+            {
+                DepartmentName = "IT",
+                Employees = new List<Employee>
+                {
+                    new Employee { EmployeeName = "Maruf" },
+                    new Employee { EmployeeName = "Zair" }
+                }
+            };
+
+            var jsonSerializer = new JSONSerializer(new FileSystem());
+
+            jsonSerializer.Serialize(departmnent, "json.json");
+
+            var result = jsonSerializer.Desirialize<Department>("json.json");
+
+            Assert.Equal(departmnent.DepartmentName, result.DepartmentName);
+        }
+    }
+}
