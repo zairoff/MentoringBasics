8cc30f727354a80fbd2274263ff0982fd860b177
Maruf <marufzoiroff@gmail.com>
2022-05-27 15:09:15 +0500

    implemented XmlSerializer

---
+++ Serialization/XMLSerializer.cs
@@ -0,0 +1,31 @@
+﻿using Serialization.Contract;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    public class XMLSerializer : ISerialize
+    {
+        private readonly IFileSystem _fileSystem;
+
+        public XMLSerializer(IFileSystem fileSystem)
+        {
+            _fileSystem = fileSystem;
+        }
+
+        public T Desirialize<T>(string path)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            using var stream = _fileSystem.OpenFileStream(path);
+            T result = (T)xmlSerializer.Deserialize(stream);
+
+            return result;
+        }
+
+        public void Serialize<T>(T entity, string path)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            using var stream = _fileSystem.CreateFileStream(path);
+            xmlSerializer.Serialize(stream, entity);
+        }
+    }
+}
