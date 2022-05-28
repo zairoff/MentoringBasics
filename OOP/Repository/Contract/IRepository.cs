01990fb6c815e6925aabdcee5e61bdbfdc1a3f3e
Maruf <marufzoiroff@gmail.com>
2022-05-28 11:54:53 +0500

    created generic repository

---
+++ OOP/Repository/Contract/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using OOP.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace OOP.Repository.Contract
+{
+    public interface IRepository<T> where T : IDocument
+    {
+        Task<IEnumerable<T>> FindAsync(Func<T, bool> filter);
+    }
+}
