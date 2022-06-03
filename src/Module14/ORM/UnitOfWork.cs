df6e1cbf6b05e8c64c3126bfff005feb2bc6ef54
Maruf <marufzoiroff@gmail.com>
2022-06-03 15:29:13 +0500

    implememted unitofwork

---
+++ src/Module14/ORM/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using ORM.Context;
+using System.Threading.Tasks;
+
+namespace ORM
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
