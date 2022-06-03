03694f93889f1ad6cfbd871441ce7491a2d3c833
Maruf <marufzoiroff@gmail.com>
2022-06-03 15:27:25 +0500

    implemented product and oder repo

---
+++ src/Module14/ORM/OrderRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ORM.Context;
+using ORM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ORM
+{
+    public class OrderRepository : BaseRepository<Order>
+    {
+        public OrderRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async override Task<IEnumerable<Order>> GetAsync()
+        {
+            return await DbSet.Include(o => o.Product).ToListAsync();
+        }
+
+        public async override Task<IEnumerable<Order>> FindAsync(Expression<Func<Order, bool>> filter)
+        {
+            return await DbSet.Where(filter).Include(o => o.Product).ToListAsync();
+        }
+
+        public async override Task<Order> GetAsync(int id)
+        {
+            return await DbSet.Where(o => o.Id == id).Include(o => o.Product).FirstOrDefaultAsync();
+        }
+    }
+}
